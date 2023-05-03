7034f9d235eb7caa70b4e3034985fda2c01c1d39
Brandon Bays <105934277+Canada1701@users.noreply.github.com>
2023-05-03 15:17:26 -0400

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,26 @@
+/* Name: Node Class for Linked Lists
+ * Author: Brandon Bays
+ * Date : May 3 / 2023
+ * Description: A simple Node class for use with linked list data structures.
+ */
+
+// Single Linked List Node Class 
+class SingleLinkedListNode {
+  // Constructor
+  // This allows us to pass both data and the next pointer into adding a new node. But if no pointer is passed, it will set it to null for us.
+  constructor(data, next = null) { 
+    this.data = data; // Variable to store the data in
+    this.next = next; // Pointer needed for singular linked lists to point to next node
+  }
+} // End Single Linked List Node Class
+
+// Double Linked List Node Class
+class DoubleLinkedListNode {
+  // Constructor
+	// This allows us to pass both data and next/previous pointer, into adding a new node. But if no pointer is passed, it will set it to null for us.
+  constructor(data, next = null, prev = null) {
+    this.data = data; // Variable to store the data in
+    this.next = next; // Pointer needed for double linked lists to point to next node
+    this.prev = prev; // Pointer needed for double linked list traversal to point to previous node
+  }
+} // End Double Linked List Node Class
